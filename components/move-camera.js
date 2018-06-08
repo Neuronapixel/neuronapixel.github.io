10dca9ce3d71caac42352d24960057aa8b28b2d3
Cacho <github@neuronapixel.com>
2018-06-08 13:32:04 -0500

    NEW move camera component

---
+++ components/move-camera.js
@@ -0,0 +1,18 @@
+/* global AFRAME */
+
+/**
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
+ */
+AFRAME.registerComponent('move-camera', {
+  schema: {
+    on: {type: 'string'}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    console.log(this.data.target);
+  }
+
+});
