8eed173182db46fbbd4af38b4cea92b7b9b93e5f
Luis Venegas <luis.venegas@neuronapixel.com>
2021-03-28 01:13:35 -0600

    temporary position page

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -12,38 +12,6 @@
         Neuronapixel cree en el valor intrínseco de la interacción como vehículo
         para el transito de las ideas.
       </p>
-      <h5>Experimentos:</h5>
-      <ul>
-        <li>
-          WebVR
-          <ul>
-            <li><router-link to="/vr-ama">VR-ama</router-link></li>
-            <li>
-              <router-link to="/memoria-virtual">Memoria Virtual</router-link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          VR
-        </li>
-        <li>
-          Processing
-          <ul>
-            <li>
-              <router-link to="/neurona-tornasol">Neurona Tornasol</router-link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          Arduino
-        </li>
-        <li>
-          Kinect
-        </li>
-        <li>
-          Web
-        </li>
-      </ul>
     </div>
   </div>
 </template>
