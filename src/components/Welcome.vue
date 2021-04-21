ef382738f7f2cf2cfc5672e2b518b14954f7a424
Luis Venegas <luis.venegas@neuronapixel.com>
2021-04-20 23:51:11 -0500

    Translate Text

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,13 +4,13 @@
       <div class="column"></div>
       <h3 style="text-align:center;">Neuronapixel</h3>
       <p>
-        Web development, scripting, hacking, coding, creatividad; la multimedia
-        como lenguaje de incontables interpretaciones en la que cada experiencia
-        es un mensaje diferente.
+        Web development, scripting, coding, creativity; multimedia as a language
+        of countless interpretations in which each experience is a different
+        message.
       </p>
       <p>
-        Neuronapixel cree en el valor intrínseco de la interacción como vehículo
-        para el transito de las ideas.
+        Neuronapixel believes in the intrinsic value of interaction as a vehicle
+        for the transit of ideas.
       </p>
     </div>
   </div>
