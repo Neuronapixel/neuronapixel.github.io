8c4d545e6a6390ca60bcef1a535ea9990d9b4e65
Luis Venegas <luis.venegas@neuronapixel.com>
2021-09-01 00:58:05 -0500

    - Adds Navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/vr-ama">VR-ama</router-link>
-      <router-link to="/memoria-virtual">Memoria virtual</router-link>
-      <router-link to="/neurona-tornasol">Neurona Tornasol</router-link>
-    </div>
+    <navigation />
     <router-view />
   </div>
 </template>
-
+<script>
+import Navigation from "@/components/NavigationBar.vue";
+export default {
+  name: "App",
+  components: {
+    Navigation
+  }
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -18,18 +20,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
