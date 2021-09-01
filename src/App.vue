d67acd67c8f581cf7705afa352ca0c984b84fd40
Luis Venegas <luis.venegas@neuronapixel.com>
2021-09-01 00:42:22 -0500

    - Menu tags Added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/vr-ama">VR-ama</router-link>
+      <router-link to="/memoria-virtual">Memoria virtual</router-link>
+      <router-link to="/neurona-tornasol">Neurona Tornasol</router-link>
+    </div>
     <router-view />
   </div>
 </template>
